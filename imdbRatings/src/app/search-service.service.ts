16c08adbc6acabd4187b958fe13ef73e9c1d249a
Dima Tchenak <Dimatch1@gmail.com>
2019-09-28 22:09:28 +0300

    added env vars

--- imdbRatings/src/app/search-service.service.ts
+++ imdbRatings/src/app/search-service.service.ts
@@ -1,3 +1,4 @@
+import { environment } from "./../environments/environment";
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
@@ -7,21 +8,11 @@ import { HttpClient } from "@angular/common/http";
 export class SearchServiceService {
   constructor(private httpClient: HttpClient) {}
 
-  public search(query: string) {
-    return this.httpClient.post(
-      "http://localhost:8080/api/search",
-      { q: query },
-      {
-        headers: { "content-type": "application/json" }
-      }
-    );
+  public search(q: string) {
+    return this.httpClient.post(environment.apiUrl + "api/search", { q }, { headers: { "content-type": "application/json" } });
   }
 
   public loadTitle(id: string) {
-    return this.httpClient.post(
-      "http://localhost:8080/api/title",
-      { id },
-      { headers: { "content-type": "application/json" } }
-    );
+    return this.httpClient.post(environment.apiUrl + "api/title", { id }, { headers: { "content-type": "application/json" } });
   }
 }
