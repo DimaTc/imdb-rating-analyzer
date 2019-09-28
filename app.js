190f4070ecd88d5d6529770152548d09d42fbb8f
Dima Tchenak <Dimatch1@gmail.com>
2019-09-28 21:57:06 +0300

    changed a little the port config

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cheerio = require("cheerio");
 const cors = require("cors");
 const app = express();
 
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.static(__dirname + "/dist/imdbRatings"));
 
